475dbdd05c3aedfc6794a07b0bb84b2d63a47d88
Brian Pattison <pattisonmb@gmail.com>
2023-09-20 17:22:32 -0500

    Initial commit

---
+++ MusicMap/MusicMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicMapApp.swift
+//  MusicMap
+//
+//  Created by Brian Pattison on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
